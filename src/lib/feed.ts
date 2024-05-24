fdd3c169fcd9efbd6be9ab3a8e55e8edbd2eeef9
Stepan Parunashvili <stepan.p@gmail.com>
2024-05-24 13:18:25 -0400

    Set up rss feeds (#3)
    * roar
    
    * roar
    
    * add rss feed
    
    * roar

---
+++ src/lib/feed.ts
@@ -0,0 +1,36 @@
+import { Feed } from "feed";
+import db from "./instantAdmin";
+
+export async function generateFeed(): Promise<Feed> {
+  const { posts } = await db.query({ posts: {} });
+  const [latestUpdatedMs] = posts
+    .map((post) => post.updatedAt)
+    .toSorted()
+    .reverse();
+  const url = "https://stopa.io";
+  const feed = new Feed({
+    title: "Stepan Parunashvili",
+    description: "Read Essays by Stepan Parunashvili",
+    id: url,
+    link: url,
+    language: "en",
+    favicon: `${url}/favicon.ico`,
+    updated: new Date(latestUpdatedMs),
+    copyright: `© ${new Date().getFullYear()} Stepan Parunashvili`,
+    feedLinks: {
+      json: `${url}/feed.json`,
+      atom: `${url}/feed.atom`,
+      rss2: `${url}/feed.rss`,
+    },
+  });
+  const orderedPosts = posts.toSorted((a, b) => b.number - a.number);
+  orderedPosts.forEach((post) => {
+    feed.addItem({
+      title: post.title,
+      link: `${url}/post/${post.number}`,
+      date: new Date(post.createdAt),
+    });
+  });
+
+  return feed;
+}
