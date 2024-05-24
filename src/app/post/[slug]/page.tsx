1059d83af5426a09305168cd8a625bb98239efe0
stopachka <stepan.p@gmail.com>
2024-05-24 14:56:39 -0400

    roar

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -3,13 +3,13 @@ import { redirect } from "next/navigation";
 import db from "@/lib/instantAdmin";
 import Link from "next/link";
 
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 export const dynamicParams = true;
 
 export async function generateStaticParams() {
   const { posts } = await db.query({ posts: {} });
   return posts.map((post) => ({
-    slug: post.number.toString()
+    slug: post.number.toString(),
   }));
 }
 
@@ -18,14 +18,22 @@ function msToFriendlyStr(ms: number) {
   return d.toLocaleDateString("en-US", { month: "long", year: "numeric" });
 }
 
-export async function generateMetadata({ params }: { params: { slug: string } }) {
-  console.log(`Generating: metadata/${params.slug}: ${new Date().toISOString()}`);
-  const { posts: [post] } = await db.query({
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  console.log(
+    `Generating: metadata/${params.slug}: ${new Date().toISOString()}`
+  );
+  const {
+    posts: [post],
+  } = await db.query({
     posts: {
       $: {
-        where: { number: +params.slug }
-      }
-    }
+        where: { number: +params.slug },
+      },
+    },
   });
   return {
     title: post.title || `Stepan Parunashvili`,
@@ -37,7 +45,7 @@ export default async function Post({ params }: { params: { slug: string } }) {
   const result = await db.query({
     posts: {
       $: { where: { number: +params.slug } },
-      body: {}
+      body: {},
     },
   });
   const [post] = result.posts;
@@ -52,19 +60,23 @@ export default async function Post({ params }: { params: { slug: string } }) {
           <Link href="/" className="text-black visited:text-black">
             Stepan <span className="hidden md:inline">Parunashvili</span>
           </Link>
-          <Link href="https://twitter.com/stopachka" target="_blank">
-            Twitter
-          </Link>
           <Link href="https://instantdb.com" target="_blank">
             Instant
+          </Link>
+          <Link href="https://twitter.com/stopachka" target="_blank">
+            Twitter
           </Link>
         </div>
       </header>
       <div className="prose prose-h1:mt-8 prose-h1:mb-4 prose-h2:mt-4 prose-h2:mb-2 mx-auto">
         <h1>{post.title}</h1>
-        <div className="text-gray-500 text-sm">{msToFriendlyStr(post.createdAt)}</div>
+        <div className="text-gray-500 text-sm">
+          {msToFriendlyStr(post.createdAt)}
+        </div>
         <div dangerouslySetInnerHTML={{ __html: html }} />
-        <div className="text-gray-500 italic text-center">Powered by <a href="https://instantdb.com">Instant</a></div>
+        <div className="text-gray-500 italic text-center">
+          Powered by <a href="https://instantdb.com">Instant</a>
+        </div>
       </div>
     </div>
   );
