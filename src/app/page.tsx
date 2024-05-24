b5262ac761a53f1b39e2e27e09e4fb15de575839
Stepan Parunashvili <stepan.p@gmail.com>
2024-05-24 14:57:51 -0400

    new header (#4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import db from "@/lib/instantAdmin";
 import Link from "next/link";
 
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 
 export default async function Home() {
   console.log(`Rendering: /: ${new Date().toISOString()}`);
@@ -12,26 +12,59 @@ export default async function Home() {
       <header className="mb-2">
         <div className="flex space-x-2 font-medium">
           <Link href="/" className="text-black visited:text-black">
-            Stepan <span className="hidden md:inline">Parunashvili</span>
-          </Link>
-          <Link href="https://twitter.com/stopachka" target="_blank">
-            Twitter
+            Stepan Parunashvili
           </Link>
         </div>
       </header>
       <div className="space-y-4">
         <div className="space-y-1">
-          <h2 className="font-bold text-gray-500">Startup</h2>
-          <p><Link href="https://instantdb.com" target="_blank" >Instant</Link>: Build real-time and offline apps</p>
+          <h2 className="text-gray-500">My life's work</h2>
+          <p>
+            <span>
+              <Link
+                className="font-medium"
+                href="https://instantdb.com"
+                target="_blank"
+              >
+                Instant
+              </Link>
+              : Build real-time and offline apps
+            </span>
+          </p>
         </div>
         <div className="space-y-1">
-          <h2 className="font-bold text-gray-500">Projects</h2>
-          <p><Link href="https://consistent.fit" target="_blank">Consistent</Link>: Make fitness part of your identity</p>
-          <p><Link href="https://zeneca.io/stopa" target="_blank">Zeneca</Link>: Share your favorite books</p>
-          <p><Link href="https://jobsearch.dev" target="_blank">Jobsearch.dev</Link>: Senior and staff interview prep</p>
+          <h2 className="text-gray-500">Projects</h2>
+          <p>
+            <Link href="https://consistent.fit" target="_blank">
+              Consistent
+            </Link>
+            : Make fitness part of your identity
+          </p>
+          <p>
+            <Link href="https://zeneca.io" target="_blank">
+              Zeneca
+            </Link>
+            : Share your favorite books
+          </p>
+          <p>
+            <Link href="https://jobsearch.dev" target="_blank">
+              Jobsearch.dev
+            </Link>
+            : Senior and staff interview prep
+          </p>
         </div>
-        <div>
-          <h2 className="font-bold mb-1 text-gray-500">Essays</h2>
+        <div className="inline-flex space-x-2">
+          <h2 className="text-gray-500 italic">Socials: </h2>
+          <Link href="https://twitter.com/stopachka" target="_blank">
+            twitter
+          </Link>
+          <span>/</span>
+          <Link href="https://twitter.com/stopachka" target="_blank">
+            favorite books
+          </Link>
+        </div>
+        <div className="space-y-1">
+          <h2 className="text-gray-500">Essays</h2>
           <div className="space-y-4">
             {orderedPosts.map((post) => {
               return (
